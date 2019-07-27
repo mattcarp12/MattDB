b763ec24fd1bbdb165cae08fe1f44705064c0de2
mcarpenter <mattcarp88@gmail.com>
2019-07-27 07:27:56 -0400

    add functionality test

---
+++ test/studentClient/simpledb/TestBasicFunctionality.java
@@ -0,0 +1,35 @@
+package studentClient.simpledb;
+
+import java.io.File;
+import java.io.IOException;
+
+import mattdb.server.MattDB;
+import mattdb.server.Startup;
+
+public class TestBasicFunctionality {
+
+    public static void deleteDbDir() {
+        String homedir = System.getProperty("user.home");
+        File dbDirectory = new File(homedir, "TestDB");
+        if (dbDirectory.exists()) {
+            for(String file : dbDirectory.list()) {
+                new File(dbDirectory, file).delete();
+            }
+            dbDirectory.delete();
+        }
+    }
+
+    public static void main(String[] args) {
+        deleteDbDir();
+        String[] dbName = {"TestDB"};
+        try {
+            Startup.main(dbName);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        CreateStudentDB.main(new String[]{});
+
+    }
+
+
+}
